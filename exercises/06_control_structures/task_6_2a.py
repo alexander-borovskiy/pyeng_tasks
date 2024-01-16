1c2ee67153d84aed8dec4013ff07695abb9dfeb3
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-01-16 21:21:38 +0300

    сделал задание 6.2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите IP-адрес в формате 10.0.1.1: ")
+if ip.count(".") == 3:
+    oct = []
+    for i in ip.split("."):
+        if i.isdigit() and 0 <= int(i) <= 255:
+            oct.append(int(i))
+    if len(oct) == 4:
+        if 1 <= oct[0] <= 223:
+            print("unicast")
+        elif 224 <= oct[0] <= 239:
+            print("multicast")
+        elif ip == "255.255.255.255":
+            print("local broadcast")
+        elif ip == "0.0.0.0":
+            print("unassigned")
+        else:
+            print("unused")
+    else:
+        print("Неправильный IP-адрес")
+else:
+    print("Неправильный IP-адрес")
