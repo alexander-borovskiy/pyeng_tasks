38c89832f553a1f09d060055824a205e7b03c837
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-01-24 20:45:00 +0300

    сделал задание 9.3

--- exercises/09_functions/task_9_3.py
+++ exercises/09_functions/task_9_3.py
@@ -23,3 +23,47 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+from pprint import pprint
+
+def get_int_vlan_map(config_filename):
+    """
+    config_filename - имя конфигурационного файла.
+
+    Возвращает кортеж из двух словарей:
+
+    * словарь портов в режиме access, где ключи номера портов,
+      а значения access VLAN (числа):
+    {'FastEthernet0/12': 10,
+     'FastEthernet0/14': 11,
+     'FastEthernet0/16': 17}
+
+    * словарь портов в режиме trunk, где ключи номера портов,
+      а значения список разрешенных VLAN (список чисел):
+    {'FastEthernet0/1': [10, 20],
+     'FastEthernet0/2': [11, 30],
+    'FastEthernet0/4': [17]}
+    """
+    access_check = ["interface", "access"]
+    trunk_check = ["interface", "trunk"]
+    access_config_dict = {}
+    trunk_config_dict = {}
+    with open(config_filename, "r") as f:
+        for section in f.read().split("!"):
+            if section:
+                words = section.split()
+                words_access = set(words) & set(access_check)
+                words_trunk = set(words) & set(trunk_check)
+                if words_access == set(access_check):
+                    access_config_dict[words[1]] = int(words[8])
+                elif words_trunk == set(trunk_check):
+                    vlan_list = words[10].split(",")
+                    int_vlan_list = []
+                    for vlan in vlan_list:
+                        int_vlan_list.append(int(vlan))
+                    trunk_config_dict[words[1]] = int_vlan_list
+    return access_config_dict, trunk_config_dict
+
+
+access_config, trunk_config = get_int_vlan_map("config_sw1.txt")
+print(access_config)
+print(trunk_config)
