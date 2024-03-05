55cbc6f9815a40f53cf7d172b3884c3cb3dbf8a3
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-03-05 19:33:01 +0300

    сделал задание 17.3a

--- exercises/17_serialization/task_17_3a.py
+++ exercises/17_serialization/task_17_3a.py
@@ -36,3 +36,35 @@
 в файл topology.yaml. Он понадобится в следующем задании.
 
 """
+
+import re
+import yaml
+
+
+def generate_topology_from_cdp(list_of_files, save_to_filename=None):
+    """
+    Функция обрабатывает вывод
+    команды show cdp neighbor из нескольких файлов и записывает итоговую
+    топологию в один словарь.
+    """
+    description_dict = {}
+    regex1 = re.compile(r'(?P<loc_dev>\S+)>')
+    regex2 = re.compile(r'(?P<rem_dev>\S+) +(?P<loc_intf>\S+ \S+) +\d+.+ (?P<rem_intf>\S+ \S+)')
+    for file in list_of_files:
+        with open(file) as f:
+            file_cont = f.read()
+            m1 = regex1.search(file_cont)
+            m2 = regex2.finditer(file_cont)
+            if m1 and m2:
+                description_dict[m1.group('loc_dev')] = {}
+                for m in m2:
+                    description_dict[m1.group('loc_dev')][m.group('loc_intf')] = {m.group('rem_dev'):m.group('rem_intf')}
+    if save_to_filename:
+        with open(save_to_filename, 'w') as f:
+            yaml.dump(description_dict, f)
+    return description_dict
+
+
+if __name__ == '__main__':
+    sh_cdp_n_list = ['sh_cdp_n_sw1.txt', 'sh_cdp_n_r1.txt', 'sh_cdp_n_r2.txt', 'sh_cdp_n_r3.txt', 'sh_cdp_n_r4.txt', 'sh_cdp_n_r5.txt', 'sh_cdp_n_r6.txt']
+    print(generate_topology_from_cdp(sh_cdp_n_list, 'topology.yaml'))
