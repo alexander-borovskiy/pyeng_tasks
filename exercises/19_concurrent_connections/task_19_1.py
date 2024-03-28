0f938c7dc483edc93279f6de8c72c69f32fa35cd
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-03-28 17:29:58 +0300

    сделал задание 19.1

--- exercises/19_concurrent_connections/task_19_1.py
+++ exercises/19_concurrent_connections/task_19_1.py
@@ -23,3 +23,41 @@
 а затем запустить эту функцию в разных потоках для разных
 IP-адресов с помощью concurrent.futures (это надо сделать в функции ping_ip_addresses).
 """
+
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
+
+
+def ping_ip_address(ip_address):
+    reply = subprocess.run(['ping', '-c', '3', '-n', ip_address],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           encoding='utf-8')
+    if reply.returncode == 0:
+        return True #, reply.stdout
+    else:
+        return False #, reply.stderr
+
+
+def ping_ip_addresses(ip_list, limit=3):
+    available_ip = []
+    not_available_ip = []
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result = executor.map(ping_ip_address, ip_list)
+        for ip, output in zip(ip_list, result):
+            if output:
+                available_ip.append(ip)
+            else:
+                not_available_ip.append(ip)
+    return available_ip, not_available_ip
+
+
+if __name__ == "__main__":
+    print(ping_ip_addresses(["192.168.100.1",
+                             "192.168.100.2",
+                             "192.168.100.3",
+                             "192.168.200.1",
+                             "8.8.8.8",
+                             ],
+                             limit=2))
