c639a5067ba644b78f0cf5ffc387aa1f778bfd7d
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-03-30 21:04:10 +0300

    сделал заявку 19.3

--- exercises/19_concurrent_connections/task_19_3.py
+++ exercises/19_concurrent_connections/task_19_3.py
@@ -39,11 +39,56 @@ router ospf 1
 
 Проверить работу функции на устройствах из файла devices.yaml и словаре commands
 """
+import logging
+import netmiko
+import time
+import yaml
 
-# Этот словарь нужен только для проверки работа кода, в нем можно менять IP-адреса
-# тест берет адреса из файла devices.yaml
-commands = {
-    "192.168.100.3": "sh run | s ^router ospf",
-    "192.168.100.1": "sh ip int br",
-    "192.168.100.2": "sh int desc",
-}
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format = '%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+
+def send_show_command_to_device(device, command):
+    start_msg = '===> {} Connection: {}'
+    received_msg = '<=== {} Received: {}'
+    ip = device['host']
+    logging.info(start_msg.format(datetime.now().time(), ip))
+    with netmiko.Netmiko(**device) as ssh:
+        ssh.enable()
+        result = ssh.send_command(command)
+        prompt = ssh.find_prompt()
+        logging.info(received_msg.format(datetime.now().time(), ip))
+    return f"{prompt}{command}\n{result}\n"
+
+
+def send_command_to_devices(devices, commands_dict, filename, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_list = []
+        for device in devices:
+            ip = device['host']
+            future = executor.submit(send_show_command_to_device, device, commands_dict[ip])
+            future_list.append(future)
+    with open(filename, "w") as file_to_save:
+        for f in future_list:
+            file_to_save.write(f.result())
+
+
+if __name__ == "__main__":
+    # Этот словарь нужен только для проверки работа кода, в нем можно менять IP-адреса
+    # тест берет адреса из файла devices.yaml
+    commands = {
+                "192.168.100.3": "sh run | s ^router ospf",
+                "192.168.100.1": "sh ip int br",
+                "192.168.100.2": "sh int desc",
+                }
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    send_command_to_devices(devices, commands, "send_command_to_devices.txt", limit=2)
+
+
