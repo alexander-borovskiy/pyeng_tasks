82fab778457a6f348d57c73284556900f7947ebe
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-04-14 19:46:39 +0300

    сделал задание 21.1а

--- exercises/21_textfsm/task_21_1a.py
+++ exercises/21_textfsm/task_21_1a.py
@@ -16,3 +16,19 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt
 и шаблоне templates/sh_ip_int_br.template.
 """
+import textfsm
+from pprint import pprint
+
+def parse_output_to_dict(template, command_output):
+    with open(template) as templ:
+        fsm = textfsm.TextFSM(templ)
+        result = fsm.ParseTextToDicts(command_output)
+        #result.insert(0, fsm.header)
+    return result
+
+
+if __name__ == "__main__":
+    with open("output/sh_ip_int_br.txt") as f:
+        output = f.read()
+        result = parse_output_to_dict("templates/sh_ip_int_br.template", output)
+    pprint(result)
