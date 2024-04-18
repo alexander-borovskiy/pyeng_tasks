f98bf70a903ff0cc2feede890dfe7a6cc70c1a5e
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-04-18 15:34:03 +0300

    сделал задание 21.5

--- exercises/21_textfsm/task_21_5.py
+++ exercises/21_textfsm/task_21_5.py
@@ -38,3 +38,26 @@
 Проверить работу функции на примере вывода команды sh ip int br
 и устройствах из devices.yaml.
 """
+import yaml
+from pprint import pprint
+from concurrent.futures import ThreadPoolExecutor
+from task_21_4 import send_and_parse_show_command
+
+
+def send_and_parse_command_parallel(devices, command, templates_path, limit=3):
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_list = []
+        ip_list = []
+        for device in devices:
+            ip = device["host"]
+            future = executor.submit(send_and_parse_show_command, device, command, templates_path)
+            future_list.append(future)
+            ip_list.append(ip)
+    result = {ip: f.result() for (ip, f) in zip(ip_list, future_list)}
+    return result
+
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+    pprint(send_and_parse_command_parallel(devices, "sh ip int br", templates_path="templates"))
