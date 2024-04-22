90338f5507336f3d8319966e7e5dd46183ad9397
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-04-22 14:37:27 +0300

    сделал задание 22.1а

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -8,21 +8,32 @@
 Перенести функциональность удаления "дублей" в метод _normalize.
 При этом метод __init__ должен выглядеть таким образом:
 """
+from pprint import pprint
 
 
 class Topology:
     def __init__(self, topology_dict):
         self.topology = self._normalize(topology_dict)
+        
+    def _normalize(self, topology_dict):
+        unique_network_map_dict = {}
+        for side_a, side_b in topology_dict.items():
+            if not unique_network_map_dict.get(side_b) == side_a:
+                unique_network_map_dict[side_a] = side_b
+        return unique_network_map_dict
+        
 
-
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+if __name__ == '__main__':
+    topology_example = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+                        }
+    top = Topology(topology_example)
+    pprint(top.topology)
