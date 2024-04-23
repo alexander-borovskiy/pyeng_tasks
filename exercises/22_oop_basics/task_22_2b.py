b0fb44da1e52ff394d5cadeb2e8235f354da173c
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-04-23 19:19:01 +0300

    сделал задание 22.2b

--- exercises/22_oop_basics/task_22_2b.py
+++ exercises/22_oop_basics/task_22_2b.py
@@ -31,3 +31,61 @@ In [6]: r1.send_config_commands(['interface loop55', 'ip address 5.5.5.5 255.255
 Out[6]: 'conf t\r\nEnter configuration commands, one per line.  End with CNTL/Z.\r\nR1(config)#interface loop55\r\nR1(config-if)#ip address 5.5.5.5 255.255.255.255\r\nR1(config-if)#end\r\nR1#'
 
 """
+import telnetlib
+import time
+from pprint import pprint
+from textfsm import clitable
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.telnet = telnetlib.Telnet(ip)
+        self.telnet.read_until(b"Username:")
+        self._write_line(username)
+        self.telnet.read_until(b"Password:")
+        self._write_line(password)
+        self._write_line("enable")
+        self.telnet.read_until(b"Password:")
+        self._write_line(secret)
+        self._write_line("terminal length 0")
+        time.sleep(1)
+        self.telnet.read_very_eager()
+        
+    def _write_line(self, line):
+        self.telnet.write(line.encode("utf-8") + b"\n")
+    
+    def send_show_command(self, show, parse=True, templates="templates", index="index"):
+        self._write_line(show)
+        output = self.telnet.read_until(b"#", timeout=5).decode("utf-8")
+        if parse:
+            cli_table = clitable.CliTable(index, templates)
+            attributes = {
+                  "Command": show,
+                  "Vendor": "cisco_ios",
+                  }
+            cli_table.ParseCmd(output, attributes)
+            result = [dict(zip(cli_table.header, row)) for row in cli_table]
+        else:
+            result = output.replace("\r\n", "\n")
+        return result
+    
+    def send_config_commands(self, commands):
+        self._write_line("conf t")
+        if type(commands) is str:
+            self._write_line(commands)
+        else:
+            for command in commands:
+                self._write_line(command)
+        self._write_line("end")
+        time.sleep(1)
+        output = self.telnet.read_very_eager().decode("utf-8")
+        return output.replace("\r\n", "\n")
+
+if __name__ == '__main__':
+    r1_params = {'ip': '192.168.100.1',
+                 'username': 'cisco',
+                 'password': 'cisco',
+                 'secret': 'cisco'}
+    r1 = CiscoTelnet(**r1_params)
+    print(r1.send_config_commands('logging 10.1.1.1'))
+    print(r1.send_config_commands(['interface loop55', 'ip address 5.5.5.5 255.255.255.255']))
