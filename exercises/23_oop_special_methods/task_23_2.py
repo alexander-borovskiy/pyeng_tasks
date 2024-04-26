19b623f299d027c9c8763de854702b2cf4b32040
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-04-26 15:05:16 +0300

    сделал задание 23.2

--- exercises/23_oop_special_methods/task_23_2.py
+++ exercises/23_oop_special_methods/task_23_2.py
@@ -44,3 +44,44 @@ ValueError: Возникла ошибка
 Тест проверяет подключение с параметрами из файла devices.yaml. Там должны быть
 указаны доступные устройства.
 """
+import telnetlib
+import time
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.telnet = telnetlib.Telnet(ip)
+        self.telnet.read_until(b"Username:")
+        self._write_line(username)
+        self.telnet.read_until(b"Password:")
+        self._write_line(password)
+        self._write_line("enable")
+        self.telnet.read_until(b"Password:")
+        self._write_line(secret)
+        self._write_line("terminal length 0")
+        time.sleep(1)
+        self.telnet.read_very_eager()
+        
+    def _write_line(self, line):
+        self.telnet.write(line.encode("utf-8") + b"\n")
+    
+    def send_show_command(self, show):
+        self._write_line(show)
+        output = self.telnet.read_until(b"#", timeout=5).decode("utf-8")
+        return output.replace("\r\n", "\n")
+        
+    def __enter__(self):
+        return self
+        
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.telnet.close()
+        
+
+if __name__ == '__main__':
+    r1_params = {'ip': '192.168.100.1',
+                 'username': 'cisco',
+                 'password': 'cisco',
+                 'secret': 'cisco'}
+    with CiscoTelnet(**r1_params) as r1:
+        print(r1.send_show_command("sh ip int br"))
+        raise ValueError('Возникла ошибка')
