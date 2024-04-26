13b5bdcc589c28b3b39912d2e83d7007a1c98fcb
alexander-borovskiy <alexander.borovskiy@yandex.ru>
2024-04-26 14:17:53 +0300

    сделал задание 23.1a

--- exercises/23_oop_special_methods/task_23_1a.py
+++ exercises/23_oop_special_methods/task_23_1a.py
@@ -31,3 +31,43 @@ In [12]: print(ip_list)
 [IPAddress('10.1.1.1/24')]
 
 """
+class IPAddress:
+    def __init__(self, net):
+        self._check_net(net)
+        ip, mask = net.split("/")
+        self._check_mask(mask)
+        self.mask = int(mask)
+        self._check_ip(ip)
+        self.ip = ip
+        self.net = net
+            
+    def _check_net(self, net):
+        if "/" not in net:
+            raise ValueError("Incorrect IPv4 address - нет /")
+        
+    def _check_mask(self, mask):
+        if not mask.isdigit() or not (8 <= int(mask) <= 32):
+            raise ValueError("Incorrect mask")
+        
+    def _check_ip(self, ip):
+        if "." not in ip or len(ip.split(".")) != 4:
+            raise ValueError("Incorrect IPv4 address")
+        else:
+            for octet in ip.split("."):
+                if not octet.isdigit() or not (0 <= int(octet) <= 255):
+                    raise ValueError("Incorrect IPv4 address")
+                    
+    def __str__(self):
+        return f"IP address {self.net}"
+        
+    def __repr__(self):
+        return f"IPAddress('{self.net}')"
+
+            
+if __name__ == "__main__":
+    ip1 = IPAddress('10.1.1.1/24')
+    print(str(ip1))
+    print(ip1)
+    ip_list = []
+    ip_list.append(ip1)
+    print(ip_list)
